6b9e7973ec16493812624a7fa18068a7457bcf34
Steve Gore <stephen.gore@q-ctrl.com>
2019-12-05 10:36:28 +1100

    First working Q-CTRL version (#1)
    Changed next URL on login to spawn notebook instance rather than going to home.
    Added ability to use an int value for user_id claim.
    Replaced python-jose with pyjwt which is used internally.
    Moved onto Poetry instead of setup.py
    Added Dockerfile for zero-to-jupyterhub Hub.
    Added CI/CD pipelines

--- jwtauthenticator/jwtauthenticator.py
+++ jwtauthenticator/jwtauthenticator.py
@@ -4,7 +4,8 @@ from jupyterhub.auth import LocalAuthenticator
 from jupyterhub.utils import url_path_join
 from tornado import gen, web
 from traitlets import Unicode, Bool
-from jose import jwt
+import jwt
+
 
 class JSONWebTokenLoginHandler(BaseHandler):
 
@@ -22,65 +23,70 @@ class JSONWebTokenLoginHandler(BaseHandler):
         tokenParam = self.get_argument(param_name, default=False)
 
         if auth_header_content and tokenParam:
-           raise web.HTTPError(400)
+            raise web.HTTPError(400)
         elif auth_header_content:
-           if header_is_authorization:
-              # we should not see "token" as first word in the AUTHORIZATION header, if we do it could mean someone coming in with a stale API token
-              if auth_header_content.split()[0] != "bearer":
-                 raise web.HTTPError(403)
-              token = auth_header_content.split()[1]
-           else:
-              token = auth_header_content
+            if header_is_authorization:
+                # we should not see "token" as first word in the AUTHORIZATION header, if we do it could mean someone coming in with a stale API token
+                if auth_header_content.split()[0] != "Bearer":
+                    raise web.HTTPError(403)
+            else:
+                token = auth_header_content
         elif auth_cookie_content:
-           token = auth_cookie_content
+            token = auth_cookie_content
         elif tokenParam:
-           token = tokenParam
+            token = tokenParam
         else:
-           raise web.HTTPError(401)
+            raise web.HTTPError(401)
 
-        claims = "";
+        claims = ""
         if secret:
             claims = self.verify_jwt_using_secret(token, secret, audience)
         elif signing_certificate:
-            claims = self.verify_jwt_with_claims(token, signing_certificate, audience)
+            claims = self.verify_jwt_using_certificate(token, signing_certificate, audience)
         else:
            raise web.HTTPError(401)
+        self.log.info("Claims: %s", claims)
 
         username = self.retrieve_username(claims, username_claim_field)
         user = self.user_from_username(username)
         self.set_login_cookie(user)
+        # self._set_cookie("jupyterhub-jwt", token, False)
 
-        _url = url_path_join(self.hub.server.base_url, 'home')
+        _url = url_path_join(self.hub.server.base_url, 'spawn')
         next_url = self.get_argument('next', default=False)
         if next_url:
              _url = next_url
 
         self.redirect(_url)
 
-    @staticmethod
-    def verify_jwt_with_claims(token, signing_certificate, audience):
-        # If no audience is supplied then assume we're not verifying the audience field.
-        if audience == "":
-            opts = {"verify_aud": False}
-        else:
-            opts = {}
+    def verify_jwt_using_certificate(self, token, signing_certificate, audience):
         with open(signing_certificate, 'r') as rsa_public_key_file:
-            return jwt.decode(token, rsa_public_key_file.read(), audience=audience, options=opts)
+            secret = rsa_public_key_file.read()
+            return self.verify_jwt_using_secret(token, secret, audience)            
 
-    @staticmethod
-    def verify_jwt_using_secret(json_web_token, secret, audience):
+    def verify_jwt_using_secret(self, token, secret, audience):
         # If no audience is supplied then assume we're not verifying the audience field.
         if audience == "":
-            opts = {"verify_aud": False}
-        else:
-            opts = {}
-        
-        return jwt.decode(json_web_token, secret, algorithms=list(jwt.ALGORITHMS.SUPPORTED), audience=audience, options=opts)
+            audience = None
+            
+        try:
+            return jwt.decode(token, secret, algorithms=['HS256','RS256'], audience=audience)
+        except jwt.ExpiredSignatureError:
+            self.log.error("Token has expired")
+        except jwt.PyJWTError as ex:
+            self.log.error("Token error - %s", ex)
+        except Exception as ex:
+            self.log.error("Could not decode token claims - %s", ex)
+        raise web.HTTPError(403)  
 
-    @staticmethod
-    def retrieve_username(claims, username_claim_field):
+    def retrieve_username(self, claims, username_claim_field):
         # retrieve the username from the claims
         username = claims[username_claim_field]
+
+        # Our system returns the username as an integer - convert to string
+        if not isinstance(username, str):
+            username = "%s" % username
+
         if "@" in username:
             # process username as if email, pull out string before '@' symbol
             return username.split("@")[0]
